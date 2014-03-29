0ea7d1d6075ab3a5d732a7efb511de8fbbb91548
Joey Gouly <joey.gouly@gmail.com>
2014-03-29 16:00:11 +0000

    Add an empty JSRunner class.
    Also add jjtimer-core as a git submodule.

---
+++ jjtimerOSX/JSRunner.cpp
@@ -0,0 +1,9 @@
+//
+//  JSRunner.cpp
+//  jjtimerOSX
+//
+//  Created by Joey Gouly on 29/03/2014.
+//  Copyright (c) 2014 jjtimer. All rights reserved.
+//
+
+#include "JSRunner.h"
---
+++ jjtimerOSX/JSRunner.h
@@ -0,0 +1,16 @@
+//
+//  JSRunner.h
+//  jjtimerOSX
+//
+//  Created by Joey Gouly on 29/03/2014.
+//  Copyright (c) 2014 jjtimer. All rights reserved.
+//
+
+#ifndef __jjtimerOSX__JSRunner__
+#define __jjtimerOSX__JSRunner__
+
+class JSRunner {
+
+};
+
+#endif /* defined(__jjtimerOSX__JSRunner__) */
